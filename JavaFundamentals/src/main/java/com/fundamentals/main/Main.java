77736e9eec6273f3bb4d19f76306aef4bff04c6c
KierraB <kierrabroadnax20@gmail.com>
2024-02-15 16:17:03 -0600

    Lesson 3 - My Application Overview

--- JavaFundamentals/src/main/java/com/fundamentals/main/Main.java
+++ JavaFundamentals/src/main/java/com/fundamentals/main/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        houseExample();
+    }
+
+    // Single Comment
+    /* Buttholes only exist within the context of our minds.
+    Everything that's ever been told to us was a lie.
+    * */
+
+    public static void houseExample() {
+        House myHouse = new House();
+        myHouse.doorFunction();
+        myHouse.entranceDoorColor = "Red";
+        myHouse.foundationType = "Concrete";
+        myHouse.roofStyle = "Cedar Shingle";
+        myHouse.totalWindows = 30;
+        System.out.println(myHouse.entranceDoorColor);
     }
 
 }

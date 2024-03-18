6e82ccabf5a23a1b85700f28a9087b63793034bd
KierraB <kierrabroadnax20@gmail.com>
2024-03-18 17:32:48 -0500

    Lesson 19 - Enumerations

---
+++ JavaFundamentals/src/main/java/com/fundamentals/lessons/Lesson19.java
@@ -0,0 +1,23 @@
+package com.fundamentals.lessons;
+
+import com.fundamentals.main.House;
+
+public class Lesson19 {
+
+    public double calculateArea(double radius){
+        return Math.PI * radius * radius;
+    }
+
+    public char[] myCharExample(){
+        return new char[] {'U', 'n', 'i', 't', 'T', 'e', 's', 't'};
+    }
+
+    public boolean isGreaterExample(int valueA, int valueB){
+        return valueA > valueB;
+    }
+
+    public House myHouseExample(House house){
+        return house;
+    }
+}
+
